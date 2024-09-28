18a375eac51e697ba2d26966afcc9cb46a68cc5d
janicogyle <161659166+janicogyle@users.noreply.github.com>
2024-09-28 23:37:15 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,65 @@
+
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=utf-8");
+header("Access-Control-Allow-Methods: GET,POST,PUT,PATCH,DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: X-Requested-With,  Origin, Content-Type,");
+header("Access-Control-Max-Age: 86400");
+// ini_set('display_errors',0);
+date_default_timezone_set("Asia/Manila");
+set_time_limit(1000);
+
+$rootPath = $_SERVER["DOCUMENT_ROOT"];
+$apiPath = $rootPath . "/crud-api";
+
+require_once($apiPath .'/config/Connection.php');
+
+//Models
+require_once($apiPath .'/model/try.model.php');
+require_once($apiPath .'/model/Global.model.php');
+
+$db = new Connection();
+$pdo = $db->connect();
+
+//Model Instantiates
+$global = new GlobalMethods();
+$try = new Example($pdo, $global);
+
+$req = [];
+if (isset($_REQUEST['request']))
+    $req = explode('/', rtrim($_REQUEST['request'], '/'));
+else $req = array("errorcatcher");
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        if ($req[0] == 'try') {echo json_encode($try->hello());return;}
+        if($req[0]=='GetAllPeople'){echo json_encode($try->getAll()); return;}
+        if ($req[0] == 'Get_Single_People' && isset($req[1])) {$userId = $req[1];echo json_encode($try->getUserById($userId));return; }
+        break;
+    case 'POST':
+        $data_input = json_decode(file_get_contents("php://input"));
+        if($req[0] == 'insert_people'){echo json_encode($try->insert_people($data_input)); return;}
+        break;
+
+    case 'PUT':
+        if ($req[0] == 'udpate_people' && isset($req[1])) { $userId = $req[1]; 
+            $input = json_decode(file_get_contents('php://input'));
+            echo json_encode($try->update_user($userId, $input));
+            return;
+        }
+        break;
+
+    case 'DELETE':
+        if ($req[0] == 'delete_people' && isset($req[1])) {
+            $userId = $req[1]; 
+            echo json_encode($try->deleteUser($userId));
+            return;
+        }
+        break;
+
+    default:
+        echo "albert";
+        http_response_code(403);
+        break;
+        
+}
